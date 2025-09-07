1d2df8ed18c2d18aa0277a4eabfba90ee40dc5ed
Dr.J <1816087404@qq.com>
2025-09-08 00:41:10 +0800

    优化了下雨的云，添加了波纹按钮

--- Components/RainCloud.js
+++ Components/RainCloud.js
@@ -11,13 +11,13 @@ class RainCloud extends HTMLElement {
         template.innerHTML = `
         <style>
           :host {
-           --cloud-height:400px;
-           --cloud-width:400px;
+           --height:200px;
+           --width:200px;
         }
           .container {
                 position: relative;
-                width:  var(--cloud-width);
-                height: var(--cloud-height);
+                width:  var(--width);
+                height: var(--width);
                 display: flex;
                 justify-content: center;
                 
@@ -28,42 +28,47 @@ class RainCloud extends HTMLElement {
                 width: 80%;
                 height: 25%;
                 border-radius: 31% 100%;
-                filter: drop-shadow( calc(var(--cloud-width) * 0.02) calc(var(--cloud-height) * 0.02) 0 #0005);
-                animation: animateCloud 2s steps(1) infinite;
+                filter: drop-shadow( calc(var(--width) * 0.02) calc(var(--width) * 0.02) 0 #0005);
+                animation: animateCloud 4s ease-in-out infinite alternate;
             }
             @keyframes animateCloud {
-                // 0% {
-                //     filter: drop-shadow(calc(var(--cloud-width) * 0.02) calc(var(--cloud-height) * 0.02) 0 #0001) drop-shadow(0 0 0 #fff) brightness(1);
-                // }
-                // 95% {
-                //     filter: drop-shadow(calc(var(--cloud-width) * 0.02) calc(var(--cloud-height) * 0.02) 0 #0001) drop-shadow(0 0 50px #fff5) brightness(10);
-                // }
+                0% {
+                    filter: drop-shadow(calc(var(--width) * 0.02) calc(var(--width) * 0.02) 0 #0001) drop-shadow(0 0 0 #fff) brightness(1);
+                }
+
+                50% {
+                    filter: drop-shadow(calc(var(--width) * 0.02) calc(var(--width) * 0.02) 0 #0001) drop-shadow(0 0 50px #fff5) brightness(1.5);
+                }
+
+                100% {
+                    filter: drop-shadow(calc(var(--width) * 0.02) calc(var(--width) * 0.02) 0 #0001) drop-shadow(0 0 0 #fff) brightness(1);
+                }
             }
             .container .cloud::before {
                 content: '';
                 position: absolute;
-                top: calc(var(--cloud-height) * -0.125);
-                left: calc(var(--cloud-width) * 0.1);
-                width: calc(var(--cloud-width) * 0.275);
-                height: calc(var(--cloud-height) * 0.275);
+                top: calc(var(--width) * -0.125);
+                left: calc(var(--width) * 0.1);
+                width: calc(var(--width) * 0.275);
+                height: calc(var(--width) * 0.275);
                 border-radius: 50%;
                 background: #484f59;
-                box-shadow: calc(var(--cloud-height) * 0.225) calc(var(--cloud-height) * -0.025) 0 calc(var(--cloud-height) * 0.075) #484f59;
+                box-shadow: calc(var(--width) * 0.225) calc(var(--width) * -0.025) 0 calc(var(--width) * 0.075) #484f59;
             }
             .container .cloud::after {
                 content: '';
                 position: absolute;
                 inset: 0;
                 background: #484f59;
-                border-radius: calc(var(--cloud-height) * 0.25);
+                border-radius: calc(var(--width) * 0.25);
                 z-index: 1000;
             }
             .container .cloud .drop {
                 position: absolute;
-                top: calc(var(--cloud-height) * 0.1);
+                top: calc(var(--width) * 0.1);
                 background: #05a2eb;
-                width: calc(var(--cloud-width) * 0.05);
-                height: calc(var(--cloud-height) * 0.025);
+                width: calc(var(--width) * 0.05);
+                height: calc(var(--width) * 0.025);
                 transform: bottom;
                 animation: animate 2s linear infinite;
             }
@@ -72,14 +77,16 @@ class RainCloud extends HTMLElement {
                     transform: translateY(0) scaleY(1);
                 }
                 70% {
-                    transform: translateY(calc(var(--cloud-height) * 0.9)) scaleY(1);
+                    transform: translateY(calc(var(--height) * 0.9)) scaleY(1);
                 }
                 80% {
-                    transform: translateY(calc(var(--cloud-height) * 0.9)) scaleY(0.2);
+                    transform: translateY(calc(var(--height) * 0.9)) scaleY(0.2);
                 }
+                /* 回弹一点点然后纵向压缩，横向扩散 */    
                 100% {
-                    transform: translateY(calc(var(--cloud-height) * 0.9)) scaleY(0) scaleX(15);
+                    transform: translateY(calc(var(--height) * 0.899)) scaleY(0) scaleX(15);
                 }
+            
             }
         </style>
         <div class="container">
@@ -94,9 +101,9 @@ class RainCloud extends HTMLElement {
         const rain = () => {
             let cloud = this.shadowRoot.querySelector('.cloud');
 
-            // 云的实际宽度（已经是 --cloud-width 的 80%）
+            // 云的实际宽度（已经是 --width 的 80%）
             let effectiveWidth = cloud.offsetWidth;
-            let effectiveHeight = parseFloat(getComputedStyle(this).getPropertyValue('--cloud-height'));
+            // let effectiveHeight = parseFloat(getComputedStyle(this).getPropertyValue('--height'));
 
             let e = document.createElement('div');
             e.classList.add('drop');
@@ -106,7 +113,7 @@ class RainCloud extends HTMLElement {
 
             // 雨滴宽高比例（相对云大小，而不是固定像素）
             let width = effectiveWidth * 0.01 * (0.5 + Math.random());   // 0.5% ~ 1.5% 云宽度
-            let height = effectiveHeight * 0.1 * (0.5 + Math.random());  // 5% ~ 15% 云高度
+            let height = effectiveWidth * 0.1 * (0.5 + Math.random());  // 5% ~ 15% 云高度
 
             // 动画时长（1s ~ 1.5s）
             let duration = (1 + Math.random() * 0.5) + 's';
---
+++ Components/RippleButton.js
@@ -0,0 +1,83 @@
+class RippleButton extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+
+        const template = document.createElement("template");
+        template.innerHTML = `
+        <style>
+        :host {
+           --height:200px;
+           --width:200px;
+           --start-color:#755bea;
+           --end-color:#ff72c0;
+        }
+           .button {
+                position: relative;
+                padding: calc(var(--height) * 0.025) calc(var(--width) * 0.125);
+                color: #fff;
+                background: linear-gradient(90deg, var(--start-color), var(--end-color));
+                border-radius: 9000px;
+                overflow: hidden;
+                cursor: pointer;
+                font-size: calc(min(var(--width), var(--height)) * 0.07);
+                 /* Chrome / Safari / Edge */
+                -moz-user-select: none;
+                /* Firefox */
+                -ms-user-select: none;
+                /* IE10+ */
+                user-select: none;
+                /* 标准写法 */
+            }
+            .button span {
+                position: absolute;
+                width: calc(var(--width) * 0.05);
+                height: calc(var(--height) * 0.05);
+                background-color: #fff;
+                border-radius: 50%;
+                pointer-events: none;
+                transform: translate(-50%, -50%);
+                animation: wave 1s linear infinite;
+                
+            }
+            @keyframes wave {
+                0% {
+                    width: 0;
+                    height: 0;
+                    opacity: .5;
+                }
+                /* width和height决定波纹扩散速度 */
+                100% {
+                    width: calc(max(var(--width), var(--height)) * 1.25);;
+                    height: calc(max(var(--width), var(--height)) * 1.25);;
+                    opacity: 0;
+                }
+            }
+        </style>
+         <div class="button"><slot></slot></div>
+      `;
+        // 将模板内容克隆到 Shadow Root
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+
+
+        const button = this.shadowRoot.querySelector('.button');
+
+        button.onclick = (e) => {
+            const rect = e.currentTarget.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+
+            const mask = document.createElement('span');
+            mask.style.cssText = `left: ${x}px; top: ${y}px`;
+            button.append(mask);
+            setTimeout(() => mask.remove(), 1000);
+        }
+
+
+
+
+    }
+}
+
+customElements.define("ripple-button", RippleButton);
